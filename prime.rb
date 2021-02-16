0de719e9aaf4f4c823dd0c0cd2bd12b91b7bc29d
Jouse then <jthen93@gmail.com>
2021-02-16 20:43:27 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+require 'pry'
+def prime?(number)
+  (2..(number - 1)).each do |num| 
+    if number % num == 0 || number < 0
+      binding.pry
+      return false
+    end
+  end
+  return true
+end
